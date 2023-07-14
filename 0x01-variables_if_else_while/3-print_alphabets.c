36ecd8bbf5f8437a7754a2a29a73319672376357
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-14 16:47:28 -0700

    adding

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+/* print lowercase alphabet */
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+/* print uppercase alphabet */
+for (letter = 'A'; letter <= 'Z'; letter++)
+{
+putchar(letter);
+}
+/* print newline character */
+putchar('\n');
+return (0);
+}
