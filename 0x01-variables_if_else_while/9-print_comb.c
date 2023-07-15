4f68795883952394c949f652c1f35c4cf3ccc6ae
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-14 17:04:08 -0700

    adding

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,14 @@
  */
 int main(void)
 {
-int first_digit, second_digit;
-for (first_digit = 0; first_digit < 10; first_digit++)
+int i;
+for (i = 0; i <= 9; i++)
 {
-for (second_digit = first_digit; second_digit < 10; second_digit++)
-{
-putchar(first_digit + '0');
-putchar(second_digit + '0');
-if (first_digit != 9 || second_digit != 9)
+putchar(i + '0');
+if (i != 9)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
