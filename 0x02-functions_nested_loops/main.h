0b9ce694c51a25238a864338d5ab7b518f4b55d4
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-18 16:55:02 -0700

    adding

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,18 +11,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-/**
- * print_alphabet - This is to print letters
- * Return:0
-*/
-
-void print_alphabet(void)
-{
-char d = 'a';
-while (d <= 'z')
-{
-_putchar(d);
-d++;
-}
-_putchar('\n');
-}
