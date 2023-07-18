c898b8ca8d0b9c02e238079da1d4f364eff96676
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-18 16:22:56 -0700

    adding

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
+
 /**
- * main - This is just a main progrma
- */
-/**
-* _putchar - writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
+ * Return:On success 1.
  * On error, -1 is returned, and errno is set appropriately.
 */
 int _putchar(char c);
 
 /**
  * main - This is just a main progrma
- */
+ * Return:0
+*/
 
 int main(void)
 {
