0cc1a9ba141288cf02297f2bccd10f44ee3c603e
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-18 16:42:31 -0700

    adding

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,15 @@
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return:On success 1.
- * On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c);
-
-/**
- *main - This ismain func
- * Return:0
-*/
-int main(void)
-{
-/**
  * print_alphabet - This is to print letters
  * Return:0
-*/	
-print_alphabet();
-return (0);
+*/
+
+void print_alphabet(void)
+{
+char d = 'a';
+while (d <= 'z')
+{
+_putchar(d);
+d++;
+}
+_putchar('\n');
 }
