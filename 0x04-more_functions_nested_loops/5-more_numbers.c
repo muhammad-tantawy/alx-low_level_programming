6bf9a06f168fef4e1b9722429a69432b7952f537
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-20 12:23:34 -0700

    adding

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *more_numbers - function prints numbers from 0 to 14 followd by a new line
+ *
+ *
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+	for (a = 0 ; a < 3 ; a++)
+	{
+		for (b = 0 ; b <=14 ; b++)
+		{
+			_putchar(b + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}	
