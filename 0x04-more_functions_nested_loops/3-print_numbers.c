af8e270592b6f08a1859ac7f04efb49cdd290299
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-20 11:39:01 -0700

    adding

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9 followed by a line
+ * Return:void
+*/
+
+void print_numbers(void)
+{
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		_putchar('a');
+	}
+	_putchar('/n');
+}
