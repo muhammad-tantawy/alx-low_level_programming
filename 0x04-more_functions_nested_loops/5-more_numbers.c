5cf6135c95ed4c09260a77533b086a950cc30c32
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-20 12:33:53 -0700

    adding

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,12 +15,17 @@ void more_numbers(void)
 
 	{
 		for (b = 0 ; b <= 14 ; b++)
+		{	
+			if (b > 9)
+			
+			{
+                		_putchar(b / 10 + '0');
+			}
+            	
+			_putchar(b % 10 + '0');
 
-		{
-			_putchar(b + '0');
 		}
 
-		_putchar('\n');
-	}
+		
 	_putchar('\n');
 }
