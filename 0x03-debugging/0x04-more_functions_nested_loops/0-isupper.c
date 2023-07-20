b41492f286a275aab81f40eeb64ee67feb941d63
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-20 07:56:53 -0700

    adding

--- 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
+++ 0x03-debugging/0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,11 @@
-#include"main.h"
-/**
- * main - checks if letter is upper case
- * Return : 0
-*/
+include "main.h"
 
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
-
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else if ( c >= 'a' &&  c <= 'z')
-	{
-		return (0);
-	}
+	return (c >= 'A' && c <= 'Z');
 }
