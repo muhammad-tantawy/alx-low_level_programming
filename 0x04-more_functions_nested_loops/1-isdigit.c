c9269c1eacbb751d6de25e5d202d238d516380d4
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-20 11:15:30 -0700

    adding

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if input is digit 0-9
+ * 
+ *@c : input to be checked
+ *Return: 0 if digit , 1 otherwise
+*/
+
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c < 9)
+	{
+		return (0)
+	}
+	else 
+	{
+		return (1)
+	}
+}
