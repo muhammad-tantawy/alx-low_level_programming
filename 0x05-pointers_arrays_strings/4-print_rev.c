51ad9a70ac31dea8d969342d9a9bf02c4e22ac5c
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-24 17:42:51 -0700

    adding

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - print in reverse
+ *
+ * @s: character used to prent in reverse
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	while (*(s + len) != '\0')
+
+	{
+		len++;
+	}
+	
+	for (int i = len - 1; i >= 0; i--)
+	{
+	
+		_putchar(*(s + i));
+	}
+	
+	_putchar('\n');
+}
