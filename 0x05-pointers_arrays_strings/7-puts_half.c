36869172c6f02a026ebeb0af57e7ce7861424526
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-24 18:37:16 -0700

    adding

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,18 +9,18 @@ void puts_half(char *str)
 	int len = 0;
 	int start;
 	int i;
-	
+
 	while (*(str + len) != '\0')
 	{
 		len++;
 	}
-	
+
 	start = len % 2 == 0 ? len / 2 : (len / 2) + 1;
-	
+
 	for (i = start; i < len; i++)
 	{
 		_putchar(*(str + i));
 	}
-	
+
 	_putchar('\n');
 }
