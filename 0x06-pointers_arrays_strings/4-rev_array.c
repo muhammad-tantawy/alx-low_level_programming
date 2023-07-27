8dd0224625af75336993e7a719c2af45b36b8002
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-27 16:37:54 -0700

    adding

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *reverse_array - function reverses content of array
+ *@a: array to be reversed
+ *n: number of elements
+ *
+ */
+
+void reverse_array(int *a, int n)
+
+{
+	int i = 0;
+	int *b;
+
+	while (i < n)
+	{
+		b[i] = a[n-i-1];
+		i++;
+	}
+	for (i = 0 ; i < n; i++)
+	{
+		a[i] = b[i];
+	}
+	return (0);
+}
