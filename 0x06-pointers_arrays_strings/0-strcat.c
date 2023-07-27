5cd2316a8eea1a0e8795b752bcc6eb4384ecf86c
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-27 14:26:52 -0700

    adding

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - function concatenates two strings
+ * @dest: destination output
+ * @src: the source string
+ * Return: returns dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	int i = 0;
+	while (src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
