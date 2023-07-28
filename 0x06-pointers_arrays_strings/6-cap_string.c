85615abeb2f1de1a9a29ce52be120430b20dad6e
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-27 18:19:23 -0700

    adding

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,10 +5,11 @@
  *
  */
 
-char *cap_string(char* str)
+char *cap_string(char *str)
 {
 	int i = 0;
 	int new_word = 1;
+
 	while (str[i] != '\0')
 	{
 		if (new_word && (str[i] >= 'a' && str[i] <= 'z'))
@@ -16,7 +17,8 @@ char *cap_string(char* str)
 			str[i] = str[i] - 'a' + 'A';
 			new_word = 0;
 		}
-		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
 			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
 			str[i] == '(' || str[i] == ')' || str[i] == '{' ||
 			str[i] == '}')
