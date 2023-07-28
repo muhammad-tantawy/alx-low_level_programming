41917e4e094d24e35b0b7de480efe2f82ecfdeb7
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-27 17:17:34 -0700

    adding

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *string_toupper - converts lower case to upper
+ *@str: string to convert
+ *@Return: returns converted string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
