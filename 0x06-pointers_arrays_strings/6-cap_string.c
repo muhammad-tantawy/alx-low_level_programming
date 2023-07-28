4ad69024b8cfab0616ade6e607e696cd752befbd
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-27 18:17:18 -0700

    adding

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+/**
+ *cap_string - capitalize all words
+ *@str: string to operate on
+ *Return: capitalized string
+ *
+ */
+
+char *cap_string(char* str)
+{
+	int i = 0;
+	int new_word = 1;
+	while (str[i] != '\0')
+	{
+		if (new_word && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] = str[i] - 'a' + 'A';
+			new_word = 0;
+		}
+		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+			str[i] == '}')
+		{
+			new_word = 1;
+		}
+		else
+		{
+			new_word = 0;
+		}
+		i++;
+		}
+
+	return (str);
+}
+
