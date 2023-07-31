97aafc532f2d34ae858610ae3bd317ba55e65519
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-31 11:08:44 -0700

    checking-memsetcode

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - function used to fill memory location
+ * @s:location to be filled
+ * @b:byte to be filled by
+ * @n:number of bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned char value;
+	unsigned char *p;
+	unsigned int i;
+	value = (unsigned char) (b & 0xFF);
+	p = (unsigned char *) s;
+	for (i = 0; i < n; i++)
+	{
+		*p++ = value;
+	}
+	
+	return(s);
+}	
