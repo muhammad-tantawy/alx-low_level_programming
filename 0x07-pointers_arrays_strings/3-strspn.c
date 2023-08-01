16a4f06564e141b8bfd464be1ea202b520d4d1ba
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-31 21:21:06 -0700

    testing_function

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,9 +10,11 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
+
 	while (*s != '\0')
 	{
 		char *p = accept;
+
 		while (*p != '\0')
 		{
 			if (*s == *p)
@@ -28,5 +30,5 @@ unsigned int _strspn(char *s, char *accept)
 		}
 		s++;
 	}
-	return(count);
+	return (count);
 }
