1ad9a11586eaf469cefdfb864b33c8c34cb8a26e
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-31 20:01:20 -0700

    testing_function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_strchr - function to search a string
+ * *_strchr - function to search a string
  *@s: string to search in
  *@c: charcter to search for
  *Return: returns the pointer
