8c91151e0e3c57ee6a26743f83b6f08170b6711d
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-07-31 19:52:29 -0700

    testing_function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,12 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != '\0' && *s != c)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
 		s++;
 	}
 
-	if (c == '\0')
+	if (*s == c)
 	{
 		return (s);
 	}
