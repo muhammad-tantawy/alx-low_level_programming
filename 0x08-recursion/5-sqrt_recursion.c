f45479317eb2bee1696ac04b9664896746c43685
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-02 18:36:31 -0700

    adding

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
-
 /**
- * _sqrt_recursion - function retruns square root of a number
- * @n: number to calculate its route
- * Return: returns the square root
  * helper - function assists to find the square root
  *@i:counter used to add up to the root
+ *Return: the next iteration
  */
 
 int helper(int n, int i)
@@ -23,6 +20,12 @@ int helper(int n, int i)
 	return (helper(n, i + 1));
 }
 
+
+/**
+ * _sqrt_recursion - function retruns square root of a number
+ * @n: number to calculate its route
+ * Return: returns the square root
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
