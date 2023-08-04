bf7b3f60249d7380a584fd37d9bda9ecf0557732
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-04 12:18:46 -0700

    adding

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - function prints the file name
+ * @argc: the number of arguments
+ * @argv: the value of the argemnts
+ * Return: 0
+ */
+
 int main(int __attribute__((unused)) argc, char *argv[])
 
 {
-	printf ("%s\n", argv[0]);	
+	printf("%s\n", argv[0]);
 	return (0);
 }
