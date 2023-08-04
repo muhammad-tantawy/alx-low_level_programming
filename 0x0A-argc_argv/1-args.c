e6d83abf4f1cf6e9acefcfcbbd9ac6087b8c0f83
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-04 12:37:48 -0700

    adding

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+
+/**
+ * main - function prints number of arguments added
+ * @argc: number of arguments
+ * @argv: content of arguments
+ * Retun: returns 0
+ */
+
 int main(int argc, char *argv[])
 
 {
