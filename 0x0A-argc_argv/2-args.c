24c09a79055504f878e3a7684eaae75cfb7d6063
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-04 12:44:13 -0700

    adding

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc:number of arguments
+ * @argv: content of the arguments
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
