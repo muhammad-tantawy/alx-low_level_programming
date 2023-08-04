1a15789f9815f08af964f92d944f8c54220d1335
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-04 12:59:56 -0700

    adding

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,14 @@
 int main(int argc, char *argv[])
 {
 	int sum;
-	printf ("%d\n", argc);
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	sum = atoi(argv[0]) * atoi( argv[1]);
+	sum = atoi(argv[1]) * atoi( argv[2]);
 	printf("%d\n", sum);
 	return (0);
 }
