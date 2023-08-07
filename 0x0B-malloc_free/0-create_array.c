117b9fd9cf7c53bbf07599535ddbccace7bf897c
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-07 14:13:29 -0700

    checking-malloc-code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,8 +12,16 @@ char *create_array(unsigned int size, char c)
 {
 	char *array;
 	int i;
-
+	
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	array = malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i <= (int)size - 1; i++)
 	{
 		array[i] = c;
