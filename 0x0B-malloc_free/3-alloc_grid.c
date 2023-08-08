e6e144b93f065f3f3607afd9c2d6edd21dc9e20f
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-08 07:06:03 -0700

    testing-2darray

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - funxtion allocates a 2 dimensional array
+ * @width: width of the required array
+ * @height: height of the needed array
+ * Return: returns NULL if failure and 0 if success
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i;
+	int x;
+	int y;
+
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+	array = malloc(height * sizeof(int *));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < width; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+		{
+			array[x][y] = 0;
+		}
+	}
+	return (array);
+}
