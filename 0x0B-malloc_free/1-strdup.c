d425387c0176d7da47518ac53e773da1589026a4
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-08 04:30:50 -0700

    checking-code

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+	char *dup;
+	int count = 0;
+	int i;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+
+	dup = malloc((count + 1) * sizeof(char));
+	for (i = 0; i < count; i++)
+	{
+		dup[i] = str[i];
+	}
+
+	if (dup == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+
+	return (dup);
+	free (dup);
+}
