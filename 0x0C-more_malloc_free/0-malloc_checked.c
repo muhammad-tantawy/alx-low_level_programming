81cc9a82e4837f2bd17ef22f92e15e4e2dfc2703
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-10 10:17:11 -0700

    adding_code

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function allocates memory using malloc
+ * @b: the size requested of memory
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+	unsigned int *array;
+
+	array = malloc(b);
+
+	if (array == NULL)
+	{
+		exit(98);
+	}
+	exit(0);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+testing more mallocs and realloc
