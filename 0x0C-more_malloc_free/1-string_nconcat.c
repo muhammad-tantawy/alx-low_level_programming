161ae126d2cf3d98fc90e188539cb9a4ae63a664
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-10 11:29:59 -0700

    adding_code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,15 +12,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
 	char *arrays;
-	int count1 = 0, count2 = 0;
+	unsigned int count1 = 0, count2 = 0;
 
 	if (s1 == NULL)
 	{
-		s1 = '';
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = '';
+		s2 = "";
 	}
 
 	while (count1 != '\n')
