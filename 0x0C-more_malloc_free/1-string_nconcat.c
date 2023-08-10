0011aa34adee66356c9392e8f3d155b4fc7f9b25
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-10 11:58:28 -0700

    adding_code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,15 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * string_nconcat - concatenates first string with n charcters second string
  *@s1: first string
  *@s2: second string
  *Return: returns pointer to the newely created array
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-
 {
 	char *arrays;
 	unsigned int count1 = 0, count2 = 0;
@@ -23,7 +20,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-
 	while (s1[count1] != '\0')
 	{
 		count1++;
@@ -32,18 +28,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		count2++;
 	}
-
 	if (n >= count2)
 	{
 		n = count2;
 	}
-
 	arrays = malloc(count1 * sizeof(s1) + n * sizeof(s2) + 1);
 	if (arrays == NULL)
 	{
 		return (NULL);
 	}
-	for (x = 0; x < count1; x++ )
+	for (x = 0; x < count1; x++)
 	{
 		arrays[x] = s1[x];
 	}
@@ -51,6 +45,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		arrays[y] = s2[y - x];
 	}
-	arrays[n + count1 ] = '\0';
+	arrays[n + count1] = '\0';
 	return (arrays);
 }
