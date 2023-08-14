a9227cbfc0b83889dcae3261f2fede6f214de136
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-14 12:07:04 -0700

    adding

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * dog - structure used to define new struc dog
+ * @name: pointer to char
+ * @age: float in struc
+ * @owner: pointer to char
+ */
 struct dog
 {
 	char *name;
