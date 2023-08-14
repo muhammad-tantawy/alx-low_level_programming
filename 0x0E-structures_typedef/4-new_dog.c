a9d94e865d7a1637151ed6599761151b384687d5
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-14 14:16:11 -0700

    adding

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,9 +3,21 @@
 #include "dog.h"
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *name2 = name;
-	char *owner2 = owner;
+	char *namecp;
+	char *ownercp;
+	int i = 0, j = 0;
 
+	while (name[i] != '\0')
+	{
+		namecp[i] = name[i];
+		i++;
+	}
+	while (owner[j] != '\0')
+	{
+		ownercp[j] = owner[j];
+		j++;
+	}
+	
 	if (new_dog == NULL)
 	{
 		return (NULL);
