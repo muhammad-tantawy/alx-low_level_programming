492450943ae39ca49219ecbeefb7f66fa1a5798e
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-14 12:56:36 -0700

    adding

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,6 +10,19 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		if (d->age == NULL)
+		{
+			printf("Age: (nil)\n");
+		}
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n")
+		}
+		
 		printf("Name:\n" , d->name);
 		printf("Age:\n", d->age);
 		printf("Owner:\n", d->owner);
