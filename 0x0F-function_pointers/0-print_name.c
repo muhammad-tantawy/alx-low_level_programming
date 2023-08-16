0bdc3202d3cc6336c6e924d15e52b269f541c7e1
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-16 10:27:14 -0700

    adding

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 /**
  * print_name - function prints name
  * @name: name to be printed
- * @(*f)(char *):pointer to a function f
+ * @f:pointer to a function f
  */
 
 void print_name(char *name, void (*f)(char *))
