e35b9fcb25a714c0db31da9d156571cf71d5245b
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-16 16:16:09 -0700

    adding

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - function specifies the function to be perofrmed
+ * @s:characterwntwered
+ * @Return: returns the functin needed to be done
+ */
+
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL}
+		};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
