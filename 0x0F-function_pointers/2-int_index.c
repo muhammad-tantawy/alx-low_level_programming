92d1ab3d209a68628b6623256b24df1f002acd85
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-16 12:18:37 -0700

    adding

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_indes - function returns the index of the found number in the array
+ * @array: array to be checked
+ * @size: size of the array
+ * @cmp: function used to compare
+ * Return: returns -1 if no element found and if size<=0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+	int i, j = 0;
+	
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == cmp(int))
+		{
+			return (i);
+			j++;
+		}
+	}
+	if (j == 0)
+	{
+		return (-1);
+	}
+}
