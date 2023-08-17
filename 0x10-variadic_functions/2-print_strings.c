71c458ab9750c7f3b1736c146ee1692aa7b5ab3f
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-17 06:53:28 -0700

    adding

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,10 +17,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char*);
-		printf("%s", str);
 		if (str == NULL)
 		{
 			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
 		}
 		if (i < n - 1 && separator != NULL)
 		{
