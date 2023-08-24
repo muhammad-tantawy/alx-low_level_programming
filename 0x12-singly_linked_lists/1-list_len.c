d9a1d11cddb11b169c02b152bbbd04378d29e8f4
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-24 12:10:06 -0700

    testing-first-push

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+
+size_t list_len(const list_t *h)
+{
+
+	long unsigned length =1;
+
+	while (h =! NULL)
+	{
+		if (h->next =! NULL)
+		{
+			h = h->next;
+			length++;
+		}
+	}
+	return (length);
+}
+
