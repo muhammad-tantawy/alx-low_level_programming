e3278323dedb3824650e8e0680511032e5ae4411
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-24 06:21:58 -0700

    adding

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,11 +15,9 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-			h = h->next;
-			size++;
-		}
+		h = h->next;
+		size++;
 	}
 	return (size);
 }
