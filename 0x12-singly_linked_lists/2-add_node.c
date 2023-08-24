6e4e949dbd15bd2ad4540c9b07998d47ee01c7a1
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-24 13:10:29 -0700

    adding

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,6 +4,7 @@
  * add_node - function adds anode at the beginning
  * @head:pointer to the address pointer of the node
  * @str: string of the new node to be added.
+ * Return: returns the address of the added node.
  */
 
 list_t *add_node(list_t **head, const char *str)
