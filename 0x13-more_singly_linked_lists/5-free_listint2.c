4efe59cceb5116507a5c132ad8018f7a8ad204b4
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-28 13:43:22 -0700

    testing-code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * free_listint2 - function frees node from a list
+ * @head: head pointer to the head element in a list
+ */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
