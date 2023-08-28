9dee756c1f347020f1a086bfff9019a1bdcea5f7
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-28 13:20:31 -0700

    testing-code

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,7 +9,7 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	listint_t const *head =h;
+	listint_t const *head = h;
 
 	while (head != NULL)
 	{
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,13 +5,13 @@ size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 	const listint_t *head = h;
-	
+
 	if (head->next  == NULL)
 	{
 		count = 1;
 	}
 	else
-	{	
+	{
 		while (head != NULL)
 		{
 			head = head->next;
