be0d7c69f3c34c5f776ea421efdf0f91d7b0f4a3
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-29 06:40:01 -0700

    adding-code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
 	listint_t *temp1 = *head;
-	unsigned int count = 0, i = 0;
+	unsigned int i = 0;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL || head == NULL || *head || NULL)
