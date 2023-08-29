01500ff2a358df7ba7f4bee49f1160fa2a9ea55a
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-29 09:15:42 -0700

    adding-code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (i = 0; i <= idx - 1; i++)
 	{
-		if (temp1 == NULL)
+		if (temp1 == NULL || temp1->next == NULL)
 		{
 			return (NULL);
 		}
