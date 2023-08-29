cb76c3a1ebc5753207ad2637da1dbbb37ad3acf4
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-29 05:21:26 -0700

    adding-code

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * sum_listint - func adds all the contents of a linked list
+ * @head: head pointer to the first node
+ * Return: returns the sum
+ */
+
 int sum_listint(listint_t *head)
 {
 	listint_t *sum;
