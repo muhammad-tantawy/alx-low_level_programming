84915c012d39b7cd39d1725f778e691e68dace63
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-31 14:35:20 -0700

    adding

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,15 +6,15 @@ void print_binary(unsigned long int n)
 
 	if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return;
 	}
 	if (n == 1)
 	{
-		putchar('1');
+		_putchar('1');
 		return;
 	}
 
 	print_binary(n >> 1);
-	putchar((n & 1) + '0');
+	_putchar((n & 1) + '0');
 }
