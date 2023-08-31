99e0f988be162eaa659ba007d20bc985e99fd6eb
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-31 14:11:02 -0700

    adding

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,18 +4,23 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int count;
+	int count;
 	unsigned int result = 0;
-
-	if (b != 0 && b != 1 )
-		return (0);
+	int y = 1;
 
 	if (b == NULL)
 		return (0);
 
-	for (count = 0; count < strlen(b); count++)
+	for (count = strlen(b) - 1; count >= 0; count--)
 	{
-		result = b[count] * (2 ^ count);
+
+		if (b[count] != '0' && b[count] != '1')
+			return (0);
+
+		result = result + (b[count] - '0') * y;
+		y = 2 * y;
+
+
 	}
 	return (result);
 }
