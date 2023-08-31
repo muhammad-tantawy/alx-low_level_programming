50925f38aace87a647d8711297578af831c1c7a4
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-31 16:46:55 -0700

    adding

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * clear_bit - func clears a bit at a given index
+ * @n: the number to operate on
+ * @index: The index which we want to change the bit at
+ * Return: 1 if success , -1 if fails
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(*n) * 8)
+		return (-1);
+
+	*n = *n & ~( 1 << index);
+	return (1);
+
