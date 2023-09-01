4bfca3ac0a45be31f0fcc19e0dbb2e5424b02113
muhammad tantawy <muhammad.tantawy@outlook.com>
2023-08-31 17:05:59 -0700

    adding

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
-
+/**
+ *flip_bits - func shows how many bits needed to flip to switch to the other number
+ *@n: the first number to be switched
+ *@m: the number to be switched to
+ *Return: returns the number of bits
+ */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 
 	unsigned long int xor = n ^ m;
 	unsigned int count = 0;
+	
 	while (xor)
 	{
 		count += xor & 1;
