ba3b1f50e33807ed72e7507c24f572fb29565054
muhammad-tantawy <134446418+muhammad-tantawy@users.noreply.github.com>
2023-09-10 13:55:57 +0000

    terstiung

--- 0x15-file_io/testt/escrow.c
+++ 0x15-file_io/testt/escrow.c
@@ -2,20 +2,22 @@
 
 int main(void)
 {
-    float base = 250;
+    float base = 450;
    // int people = 6;
     float inter;
+    float enter = 0;
     float result;
     int number_of_cycles =1;
 
-    while (base <3000)
+    while (enter <3000)
     {
         inter = base * .05 ;
         result =  (base + inter);
         base = result;
         if ( number_of_cycles %2 ==0)
             base = base + 50;
-        printf("%f\n", base);
+        enter = base + enter;
+        printf("%f\n", enter);
         number_of_cycles++;
     }
     printf (" number of cycles is %d",number_of_cycles);
