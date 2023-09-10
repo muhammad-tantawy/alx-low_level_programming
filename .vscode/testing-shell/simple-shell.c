15fa7000ef87df3c54317265acd3955db2851a58
muhammad-tantawy <134446418+muhammad-tantawy@users.noreply.github.com>
2023-09-10 13:43:15 +0000

    tetsing first shell

--- .vscode/testing-shell/simple-shell.c
+++ .vscode/testing-shell/simple-shell.c
@@ -1,10 +1,51 @@
 #include"header.h"
 
-int main (int ac, char **av, char **env)
-{
+int main(int argc, char *argv[], char *env[]) {
+char cmd[MAX_CMD_LEN];
+    char *args[MAX_ARGS];
+    pid_t pid;
+    int status;
+    int i = 0;
+    while (1) {
+    printf(">simple-shell ");
 
+    // Get the command from the user
+    if (fgets(cmd, sizeof(cmd), stdin) == NULL) {
+        break;
+    }
 
+    cmd[my_strlen(cmd) - 1] = '\0';  // remove newline at the end
 
+    // split command into arguments
+    int i = 0;
+    args[i] = strtok(cmd, " ");
+    while (args[i] != NULL) {
+        args[++i] = strtok(NULL, " ");
+    }
+
+    if (my_strcmp(args[0], "exit") == 0) {
+        break;
+    }
+
+    if ((pid = fork()) < 0) 
+    {
+        printf("Fork error\n");
+        exit(1);
+    } 
+    else if (pid == 0) 
+    {
+        if (execve(args[0], args, env) < 0) 
+        {
+            printf("Please enter full path such as \"/bin/ls\"\n");
+            exit(1);
+        }
+    } 
+    else 
+    {   
+        while (wait(&status) != pid);
+    }
+}
+    return 0;
+}
 
     
-}
