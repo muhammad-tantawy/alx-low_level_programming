b5946b98bb6d9cb8df68ea1fd20cff56bbf7a784
muhammad-tantawy <muhammad.tantawy@outlook.com>
2023-10-13 02:53:02 +0300

    adding_code

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -7,7 +7,7 @@ int sum_dlistint(dlistint_t *head)
 		return (0);
 	while (head->prev != NULL)
 		head = head->prev;
-	while (head !=NULL)
+	while (head != NULL)
 	{
 		head = head->next;
 		sum = sum + head->n;
