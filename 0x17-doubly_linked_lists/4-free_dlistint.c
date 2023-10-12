07f42fa8d55441215d4ee772e83a152e0a7f3ecd
muhammad-tantawy <muhammad.tantawy@outlook.com>
2023-10-13 02:31:52 +0300

    adding_code

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -11,7 +11,7 @@ void free_dlistint(dlistint_t *head)
 	if (head != NULL)
 		while (head->prev != NULL)
 			head = head->prev;
-	tmp = head
+	tmp = head;
 	while (tmp != NULL)
 	{
 		head = head->next;
