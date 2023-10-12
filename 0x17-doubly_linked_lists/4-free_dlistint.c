dd6cbb13ee786627ec447380d8e6a1293849e34c
muhammad-tantawy <muhammad.tantawy@outlook.com>
2023-10-13 02:28:19 +0300

    adding_code

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_dlistint - func frees the double linked list
+ * @head: the head pointer
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+}
