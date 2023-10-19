eb03d0ec9cfff69afcfb6c2540821310d25fc419
muhammad-tantawy <muhammad.tantawy@outlook.com>
2023-10-19 16:09:34 +0300

    adding_code

---
+++ 0x18-dynamic_libraries/arith.c
@@ -0,0 +1,60 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+/**
+ * add - function adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: returns the sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ ** sub - function subs two integers
+ ** @a: first integer
+ ** @b: second integer
+ ** Return: returns the difference
+ **/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ ** mul - function multiplies two integers
+ ** @a: first integer
+ ** @b: second integer
+ ** Return: returns the multiplicatoin
+ **/
+int mul(int a, int b)
+{
+	return (a * b)
+}
+/**
+ ** div - function divides two integers
+ ** @a: first integer
+ ** @b: second integer
+ ** Return: returns the divided by two integers
+ **/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ ** mod - function return the modulus two integers
+ ** @a: first integer
+ ** @b: second integer
+ ** Return: returns the modulus 
+ **/
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
